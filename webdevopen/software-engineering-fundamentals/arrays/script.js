55219c6b7127f993cc64524329f15f21095a884d
joscanoav <joscanoav@gmail.com>
2025-09-21 20:09:45 +0200

    array exercise completed

--- webdevopen/software-engineering-fundamentals/arrays/script.js
+++ webdevopen/software-engineering-fundamentals/arrays/script.js
@@ -2,4 +2,16 @@ let selectedColors = ['cristian', 'matias'];
 console.log(selectedColors);
 
 let myFavoriteFruits = ["banana", "cherry", "mango"];
-let MyLuckyNumbr
+console.log(myFavoriteFruits);
+myFavoriteFruits.push("pear");
+console.log(myFavoriteFruits);
+myFavoriteFruits.push("banana");
+console.log(myFavoriteFruits);
+console.log(myFavoriteFruits.length);
+myFavoriteFruits.pop();
+console.log(myFavoriteFruits);
+let lastFruit = myFavoriteFruits.pop();
+console.log(lastFruit);
+console.log(myFavoriteFruits);
+myFavoriteFruits.push(lastFruit);
+console.log(myFavoriteFruits);

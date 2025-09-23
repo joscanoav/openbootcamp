c33edde6a0e758a67795bac932f2522f92c8eb98
joscanoav <joscanoav@gmail.com>
2025-09-23 20:16:47 +0200

    interation started

--- webdevopen/software-engineering-fundamentals/Iterations/script.js
+++ webdevopen/software-engineering-fundamentals/Iterations/script.js
@@ -2,3 +2,8 @@ let countTo = 5;
 for (let step = 1; step <= countTo; step++){
     console.log(step);
 }
+
+let numberOfGuests= 20;
+for (let index = 1; index <= numberOfGuests; index++){
+    console.log("Here is a 🍔 for guest #" + index);
+}

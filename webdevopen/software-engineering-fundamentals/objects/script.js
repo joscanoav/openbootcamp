25c9644611773d5f6ea13df20001f7ee71b49df1
joscanoav <joscanoav@gmail.com>
2025-10-18 23:30:18 +0200

    Started objects

---
+++ webdevopen/software-engineering-fundamentals/objects/script.js
@@ -0,0 +1,11 @@
+let countTo = 5;
+for (let step = 1; step <= countTo; step++){
+    console.log(step);
+}
+
+let numberOfGuests= 20;
+for (let index = 1; index <= numberOfGuests; index++){
+    console.log("Here is a 🍔 for guest #" + index);
+}
+
+update

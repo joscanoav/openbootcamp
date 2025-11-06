a6ae679bcc5ae64963cf4d96e08bb427f85bc1e6
joscanoav <joscanoav@gmail.com>
2025-11-06 22:10:44 +0100

    update iteractions

--- webdevopen/software-engineering-fundamentals/Iterations/script.js
+++ webdevopen/software-engineering-fundamentals/Iterations/script.js
@@ -8,4 +8,20 @@ for (let index = 1; index <= numberOfGuests; index++){
     console.log("Here is a 🍔 for guest #" + index);
 }
 
-update
+//**   Drill : Loop practice */
+
+let number = 10;
+for(let i = 1; i <= number; i++){
+    console.log(i);
+}
+
+let n = 10;
+for (let i=5; i <= n;i++ ){
+    console.log(i);
+}
+
+let x = 100;
+for(let i = 0; i <= x; i=i+10){
+    console.log(i);
+}
+

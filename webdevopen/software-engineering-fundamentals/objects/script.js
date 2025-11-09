ba4142cb60bc8db4b355d7a5edfc4f7148d7a341
joscanoav <joscanoav@gmail.com>
2025-11-09 20:40:17 +0100

    Started Objets

--- webdevopen/software-engineering-fundamentals/objects/script.js
+++ webdevopen/software-engineering-fundamentals/objects/script.js
@@ -1,11 +1,9 @@
-let countTo = 5;
-for (let step = 1; step <= countTo; step++){
-    console.log(step);
-}
+let gifts = {
+    tommy: "stuffed giraffe",
+    lisa: "coloring book",
+    roberto: "baseball cap",
+    beth: "suit of armor",
+};
 
-let numberOfGuests= 20;
-for (let index = 1; index <= numberOfGuests; index++){
-    console.log("Here is a 🍔 for guest #" + index);
-}
+console.log(gifts.tommy)
 
-update

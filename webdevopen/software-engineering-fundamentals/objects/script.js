7673714ca1edf5d83cf72ac533df3a75ad2a87d5
joscanoav <joscanoav@gmail.com>
2025-11-10 13:13:20 +0100

    Objects exercises completed

--- webdevopen/software-engineering-fundamentals/objects/script.js
+++ webdevopen/software-engineering-fundamentals/objects/script.js
@@ -7,3 +7,69 @@ let gifts = {
 
 console.log(gifts.tommy)
 
+let actionFigure = {
+    name: "Super Awesome Guy",
+    heighInCentimeters: 10,
+    accessories: ["cape", "dosguise", "laser ring"],
+    limitedEdition: true,
+};
+
+console.log(actionFigure.name);
+console.log(actionFigure.limitedEdition);
+
+let myFavorites = {
+    movie: "Shrek",
+    song: "Bohemian Rhapsody",
+    food: "Lasagna",
+};
+
+console.log(myFavorites.movie);
+console.log(myFavorites.song);
+console.log(myFavorites.food);
+
+
+
+// getting information out of an object
+
+let categories = {
+        'best picture': 'Shrek',
+        'best director': 'Shrek 2'
+};
+
+// we can't do these
+
+console.log(categories["best director"]);
+categories["best picture"];
+
+
+// adding a new key/value pair
+
+let myFamily = {
+    mon: "Cynthia",
+    dad: "Paul",
+};
+
+console.log(myFamily.sister = "Lucinda");// dot notation
+myFamily["brother in law"] = "Merle";
+
+console.log(myFamily);
+
+myFamily.sister = "Lucinda";
+
+delete myFamily.sister;
+console.log(myFamily);
+
+
+
+let fruitBasket = {
+    apple: "red",
+    blackberry: "black",
+    orange: "orange",
+};
+
+
+fruitBasket.chirimoya = 'green';
+fruitBasket.granadilla = 'ocre';
+fruitBasket.mango = 'yellow';
+
+console.log(fruitBasket);

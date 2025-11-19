abadc8b9a89078a6906371f9bd2628f1bd4441bd
joscanoav <joscanoav@gmail.com>
2025-11-19 21:43:06 +0100

    Objects in a loop

--- webdevopen/software-engineering-fundamentals/objects/script.js
+++ webdevopen/software-engineering-fundamentals/objects/script.js
@@ -104,3 +104,24 @@ let students = {
 };
 
 let temp;
+
+let partyPeople = [
+    { name: "Joe", attending: false},
+    { name: "Jasmine", attending: true},
+    { name: "Julio", attending: false},
+    { name: "Julia", attending: true},
+];
+
+/* 
+    loop through each item check if the person
+    is attending if they are attending, say they are
+    partying otherwise, say they are burnt out
+*/
+
+for (let i = 0; i < partyPeople.length; i++){
+    if (partyPeople[i].attending){
+        console.log(partyPeople[i].name + " is partying!");
+    } else {
+        console.log(partyPeople[i].name + " is burnt out on partying = (");
+    }
+}

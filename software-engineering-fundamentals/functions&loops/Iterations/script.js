15d516a75f3887ef56dff1972c899e84592d9065
joscanoav <joscanoav@gmail.com>
2026-01-13 23:13:30 +0100

    function and loop created

---
+++ software-engineering-fundamentals/functions&loops/Iterations/script.js
@@ -0,0 +1,5 @@
+let friends = ["Alexandria", "Camden", "Kamila"];
+for (let i = 0; i < friends.length; i++){
+    let friend = friends[i];
+    console.log("Hello " + friend + ", I'm glad that you're here!");
+}
